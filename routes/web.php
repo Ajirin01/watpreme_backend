cf7b5d4b322898852f000157b816af4750f45dbc
Ajirin01 <mubarakolagoke@gmail.com>
2024-03-11 18:42:02 +0100

    channel added to handle webhook

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Pusher\Pusher;
 
 // Access token for your app
 // (copy token from DevX getting started page
@@ -14,6 +15,38 @@ use Illuminate\Support\Facades\Http;
 $token = env('WHATSAPP_TOKEN');
 
 // Sets server port and logs message on success
+// Route::post('/webhook', function (Request $request) use ($token) {
+//     // Parse the request body from the POST
+//     $body = $request->all();
+
+//     // Check the Incoming webhook message
+//     info(json_encode($request->all(), JSON_PRETTY_PRINT));
+
+//     // info on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
+//     if ($request->has('object')) {
+//         $entry = $request->input('entry.0');
+//         if (
+//             isset($entry['changes']) &&
+//             isset($entry['changes'][0]['value']['messages'][0])
+//         ) {
+//             $phoneNumberId = $entry['changes'][0]['value']['metadata']['phone_number_id'];
+//             $from = $entry['changes'][0]['value']['messages'][0]['from'];
+//             $msgBody = $entry['changes'][0]['value']['messages'][0]['text']['body'];
+
+//             // Send message
+//             Http::post("https://graph.facebook.com/v12.0/{$phoneNumberId}/messages?access_token={$token}", [
+//                 'messaging_product' => 'whatsapp',
+//                 'to' => $from,
+//                 'text' => ['body' => "Ack: {$msgBody}"]
+//             ]);
+//         }
+//         return response()->json([], 200);
+//     } else {
+//         // Return a '404 Not Found' if event is not from a WhatsApp API
+//         return response()->json(['error' => 'Not Found'], 404);
+//     }
+// });
+
 Route::post('/webhook', function (Request $request) use ($token) {
     // Parse the request body from the POST
     $body = $request->all();
@@ -38,6 +71,13 @@ Route::post('/webhook', function (Request $request) use ($token) {
                 'to' => $from,
                 'text' => ['body' => "Ack: {$msgBody}"]
             ]);
+
+            // Trigger Pusher event
+            $pusher = new Pusher(env('PUSHER_APP_KEY'), env('PUSHER_APP_SECRET'), env('PUSHER_APP_ID'), [
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'useTLS' => true
+            ]);
+            $pusher->trigger('whatsapp-events', 'message-received', ['message' => "Ack: {$msgBody}"]);
         }
         return response()->json([], 200);
     } else {
